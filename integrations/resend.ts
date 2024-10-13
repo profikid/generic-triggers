4e11592d03c9a9eb68b9d078f4cbc72a295246d0
Laurens Profittlich <profikid@gmail.com>
2024-10-13 22:58:15 +0700

    some new integrations

---
+++ integrations/resend.ts
@@ -0,0 +1,39 @@
+import { logger, task } from '@trigger.dev/sdk/v3';
+import { Resend } from 'resend';
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+  export const textEmail = task({
+    id: 'send',
+    run: async (payload: any) =>{
+        logger.log(payload?.subject || '<no subject>');
+        logger.log(payload?.message || '<no body>');
+        const result = await resend.emails.send({
+            from: 'no-reply@profikid.nl',
+            to: 'profikid@gmail.com',
+            subject: payload.subject,
+            text: payload.message
+        });
+
+        return {
+            ...result,
+        }
+    }
+})
+
+export const htmlEmail = task({
+    id: 'send',
+    run: async (payload: any) =>{
+        logger.log(payload?.subject || '<no subject>');
+        logger.log(payload?.message || '<no body>');
+        const result = await resend.emails.send({
+            from: 'no-reply@profikid.nl',
+            to: 'profikid@gmail.com',
+            subject: payload.subject,
+            html: payload.message
+        });
+
+        return {
+            ...result,
+        }
+    }
+})
---
+++ transformers/mdToHtml.ts
@@ -0,0 +1,20 @@
+import { logger, task } from '@trigger.dev/sdk/v3';
+import { marked } from 'marked';
+
+export const markdownToHtmlTask = task({
+  id: 'md2html',
+  run: async (payload: { markdown: string }) => {
+    // Log the input markdown text
+    logger.log(payload?.markdown || '<no markdown>');
+
+    // Convert the markdown to HTML
+    const html = marked(payload.markdown);
+
+    // Log the converted HTML text
+    logger.log('HTML', {html});
+
+    return {
+      html,
+    };
+  },
+});
